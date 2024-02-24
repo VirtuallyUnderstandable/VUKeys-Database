8f65d209bf73766a141bfacff5e9db1472918228
poseisharp <poseiayayui@gmail.com>
2024-02-24 22:37:51 +0700

    downgrade to BrailleCell only, add kana WIP

--- brailledatabase/src/main/java/com/lukeneedham/brailledatabase/Braille/SymbolDatabases/BrailleDatabaseType.java
+++ brailledatabase/src/main/java/com/lukeneedham/brailledatabase/Braille/SymbolDatabases/BrailleDatabaseType.java
@@ -14,7 +14,8 @@ public enum BrailleDatabaseType
 	SLOVAK(new BrailleDatabaseSlovak(), R.string.database_slovak, R.string.database_slovak_desc, new String[]{"slo", "slk"}),
 	TAMIL(new BrailleDatabaseTamil(), R.string.database_tamil, R.string.database_tamil_desc, "tam"),
 	SPANISH(new BrailleDatabaseSpanish(), R.string.database_spanish, R.string.database_spanish_desc, "spa"),
-	KANA(new BrailleDatabaseKana(), R.string.database_kana, R.string.database_kana_desc, "jpn");
+	KANA(new BrailleDatabaseKana(), R.string.database_kana, R.string.database_kana_desc, "kana"),
+	;
 
 	BrailleDatabase db;
 	int name;
